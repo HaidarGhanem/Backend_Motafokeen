e83fded5621938682fa6f73c7a643ac3660eff0b
HaidarGhanem <haidar23ghanem@gmail.com>
2025-06-28 23:06:20 +0300

    ready to deploy

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(session({
 }))
 
 //Middlewares
-app.use(cors({origin: 'http://localhost:5173',credentials: true}))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
