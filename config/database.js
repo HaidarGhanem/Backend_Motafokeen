3e36ce2e7dab0d9d8e26c087a6287b1de5c3a247
HaidarGhanem <haidar23ghanem@gmail.com>
2025-09-14 11:21:17 +0300

    new new info 2

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     mongoose.set('strictQuery', false)
     const conn = await mongoose.connect(
-      'mongodb+srv://school:school@school.owekosq.mongodb.net/school',
+      'mongodb+srv://haidar23ghanem:0934937147@school.iz8hbwi.mongodb.net/school',
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
